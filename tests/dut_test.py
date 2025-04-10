b04170d3b58408dee598e728db420cc33466453a
vishwaksen-1 <newtonlucky35@gmail.com>
2025-04-10 19:25:13 +0530

    added tests

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,7 +1,19 @@
 import cocotb
 from cocotb.triggers import Timer
+from random import random as rand
 
+def xor(a, b):
+    return a^b
 
 @cocotb.test()
 async def dut_test(dut):
-    assert 0, "Test Not Implemented Error"
+    dut.a.value = 0
+    dut.b.value = 0
+    await Timer(1, 'ns')
+    assert dut.y.value == 0, "Simple Test"
+    for i in range(10):
+        dut.a.value = 0 if rand() < 0.5 else 1
+        dut.b.value = 0 if rand() < 0.5 else 1
+        await Timer(1, 'ns')
+        assert dut.y.value == xor(dut.a.value, dut.b.value), f"Random Test {i+1}"
+    
